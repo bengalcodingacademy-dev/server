9f4d2b88638703fda826820723390dc79b819c2e
Sauvik Chatterjee <bengalcodingacademy@gmail.com>
2025-09-14 15:06:36 +0530

    logs added

--- src/index.js
+++ src/index.js
@@ -39,7 +39,9 @@ app.use(cors({
     'https://bengalcodingacademy.com',
     'https://www.bengalcodingacademy.com'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 app.use(rateLimit({ windowMs: 60 * 1000, max: 120 }));
 
